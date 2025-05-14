041f4a176e5dfa99456d8ca5b28bf52d6a05e4b3
Salman-S7 <sammyzed1505@gmail.com>
2025-05-15 01:21:21 +0530

    added get content and add content routes

--- backend/src/controller/crudController.ts
+++ backend/src/controller/crudController.ts
@@ -1,5 +1,7 @@
 import { Response } from "express";
 import { AuthRequest } from "../middleware/authMiddleware";
+import Content from "../model/contentModel";
+import Tag from "../model/tagModel";
 
 export const addContent = async (req: AuthRequest, res: Response) => {
   const { type, link, title, tags } = req.body;
@@ -9,5 +11,83 @@ export const addContent = async (req: AuthRequest, res: Response) => {
   if (!type || !link || !title || !tags) {
     res.status(499).json({ message: "All fields are required" });
     return;
+  }
+
+  try {
+    const existingTag = await Tag.findOne({ title: tags });
+
+    if (!existingTag) {
+      const newTag = new Tag({ title: tags });
+
+      const savedTag = await newTag.save();
+
+      const existingContent = await Content.findOne({
+        type,
+        link,
+        title,
+        tags: savedTag,
+        userId,
+      });
+
+      if (existingContent) {
+        res.status(403).json({ message: "Content already exists" });
+        return;
+      }
+
+      const newContent = new Content({
+        type,
+        link,
+        title,
+        tags: savedTag,
+        userId,
+      });
+
+      await newContent.save();
+
+      res.status(201).json({ message: "Content saved succesfully" });
+
+      return;
+    }
+
+    const newContent = new Content({
+      type,
+      link,
+      title,
+      tags: existingTag,
+      userId,
+    });
+
+    const existingContent = await Content.findOne({
+      type,
+      link,
+      title,
+      tags: newContent,
+      userId,
+    });
+
+    if (existingContent) {
+      res.status(403).json({ message: "Content already exists" });
+      return;
+    }
+
+    await newContent.save();
+
+    res.status(201).json({ message: "Content saved succesfully" });
+  } catch (error) {
+    console.log("Error while saving content", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getContent = async (req: AuthRequest, res: Response) => {
+  const userId = req.userId;
+
+  try {
+    const data = await Content.find({ userId });
+
+    res.status(200).json({ content: data });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
--- backend/src/routes/pageRoutes.ts
+++ backend/src/routes/pageRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { login, signup } from "../controller/authController";
-import { addContent } from "../controller/crudController";
+import { addContent, getContent } from "../controller/crudController";
 import { authMiddleWare } from "../middleware/authMiddleware";
 
 const router = Router();
@@ -8,5 +8,6 @@ const router = Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/content", authMiddleWare, addContent);
+router.get("/content", authMiddleWare, getContent);
 
 export default router;
